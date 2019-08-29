50425e1d0aff90b3f76f9cc6f0699979690e5087
Jeff Scott Brown <brownj@objectcomputing.com>
2019-08-29 10:46:47 -0500

    demo

---
+++ helper/src/main/java/helper/HelloController.java
@@ -0,0 +1,14 @@
+package helper;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.HttpStatus;
+
+@Controller("/hello")
+public class HelloController {
+
+    @Get("/")
+    public String index() {
+        return "Hello";
+    }
+}
